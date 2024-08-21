518ae07164e01cdf9ce1b0ff1c78b33872401bb7
SLebron <selny875@gmail.com>
2024-08-21 13:48:13 -0400

    trying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import MemberMain from './MemberMain';
 
 function App() {
- 
+    return (
+        <Router>
+          <Routes>
+            <Route path='/membermain' element={<MemberMain />} />
+          </Routes>
+        </Router>
+      );
 }
 
 export default App;
---
+++ src/LogoImg.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import tttLogo from './tttLogo.png';
+
+ function LogoImg(){
+
+    return(
+
+        <div>
+        <img src={tttLogo} alt="My Image" width='50%' height='50%'/>
+        </div>
+
+    );//end return
+
+}
+export default LogoImg;
